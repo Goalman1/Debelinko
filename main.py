33c39cca852a8c6c249250f2a878b48a4303450f
Znanec <elvisbilic@gmail.com>
2019-05-30 18:37:39 +0200

    vaja17

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import datetime
 app =  Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+        some_text = "Message from the handler."
+        current_year = datetime.datetime.now().year
+        cities = ["Boston", "Vienna", "Paris", "Berlin"]
 
+        return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 @app.route("/about")
 def about_me():
     return render_template("about.html")
@@ -14,6 +17,17 @@ def about_me():
 @app.route("/portfolio")
 def portfolio():
     return render_template("portfolio.html")
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+    return render_template("success.html")
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
